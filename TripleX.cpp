abb112110522849028982542b8d2ed3eac21f6b9
josephLeiferman <joseph.leierman@gmail.com>
2020-05-17 12:09:09 -0500

    Refactor main() and add .gitignore file

--- TripleX.cpp
+++ TripleX.cpp
@@ -2,7 +2,23 @@
 
 int main()
 {
+    // Dispaly welcome messages to the terminal
+    std::cout << "You are a secret agent breaking into a secure server room..." << std::endl;
+    std::cout << "You need to enter the correct codes to continue..." << std::endl << std::endl;
 
-    std::cout << "Hello, World!";
+    const int CodeA = 2;
+    const int CodeB = 3;
+    const int CodeC = 4;
+
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
+
+    std::cout << "+ There are 3 numbers in the code" << std::endl;
+    std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
+    std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
+
+
+    int PlayerGuess;
+
     return 0;
 }
